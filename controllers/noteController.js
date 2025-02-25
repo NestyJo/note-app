3c15a1db98123357400ffa5d63baca249b32accb
NestyJo <smartlabstech@yahoo.com>
2025-02-25 13:10:51 +0300

    Note Controller get, post, update, delete

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -1,1 +1,27 @@
 
+const Note = require('../models/Note');
+
+exports.createNote = async (req, res) => {
+    const { title, content } = req.body;
+    const note = new Note({ title, content, userId: req.userId });
+    await note.save();
+    res.status(201).json(note);
+};
+
+exports.getNotes = async (req, res) => {
+    const notes = await Note.find({ userId: req.userId });
+    res.json(notes);
+};
+
+exports.updateNote = async (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    const note = await Note.findByIdAndUpdate(id, { title, content }, { new: true });
+    res.json(note);
+};
+
+exports.deleteNote = async (req, res) => {
+    const { id } = req.params;
+    await Note.findByIdAndDelete(id);
+    res.status(204).send();
+};
